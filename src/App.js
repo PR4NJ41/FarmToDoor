2a548ad1682eca0b7cd28c67960885a06836d339
Pranjal <pranjal.rajput.che21@itbhu.ac.in>
2023-03-11 16:46:05 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes,Route} from 'react-router-dom'
 import "./App.css"
 import Homepage from "./pages/homepage/index"
+import FAQ from "./pages/faq/faq"
 
 const App = () => { 
   return (
     <>
 		<Routes>
 			<Route path="/" element={<Homepage/>}/>
+			<Route path="/faq" element={<FAQ/>}/>
 		</Routes>
     </>
   )
