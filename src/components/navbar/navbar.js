8bcc5c66e176043c4a40f7aec3a04a2efffc108a
Pranjal <pranjal.rajput.che21@itbhu.ac.in>
2023-03-11 16:46:25 +0530

    Created Navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navbar.css"
+
+const navbar = () => {
+  return (
+    <>
+		<div className='navbar'>
+			<div className='box'>Home</div>
+			<div className='box'>About Us</div>
+			<div className='box'>FAQ</div>
+		</div>
+    </>
+  )
+}
+
+export default navbar
